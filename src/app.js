8d4911b7b68dc8d116087d41e34aef277a0ce6f2
Francis <inmob.comercial@gmail.com>
2020-08-22 11:38:51 -0300

    Teste 2 concluido Bootcamp 2020

--- src/app.js
+++ src/app.js
@@ -10,33 +10,54 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+app.get("/repositories", (request, response) => { 
+
   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   const {title, url, techs} = request.body;
 
-  const repository = {
-    id: uuid(),
-    title,
-    url,
-    techs,
+  const repository = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs, 
     likes: 0,
-  }
+   }
+
   repositories.push(repository);
 
   return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-  //falta
-});
+ const { id } = request.params;
+ const {title, url, techs, likes} = request.body;
 
+ const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+ if(repositoryIndex < 0){
+   return response.status(400).json({error: 'Repository not found.'})
+ }
+ const repository = {
+   id,
+   title,
+   url,
+   techs,
+   likes: 0,
+ };
+ repositories[repositoryIndex] = repository;
+ return response.status(200).json(repository);
+});
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-  //Falta
+  const {id} = request.params;
+const repositoryIndex = repositories.findIndex(repositories => repositories.id == id)
+if(repositoryIndex < 0){
+return response.status(400).json({error: "NOT FOUND ID!!!"})
+}
+repositories.splice(repositoryIndex, 1)
+return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
